fa75272460e662f63a838f32f316423c2da99a4c
Natasha Murashev <nmurashev@gmail.com>
2013-05-24 13:28:41 -0500

    initial commit

---
+++ AppNet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppNet
+//
+//  Created by Natasha Murashev on 5/24/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppNet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppNet
+//
+//  Created by Natasha Murashev on 5/24/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
